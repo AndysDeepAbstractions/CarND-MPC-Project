f28b7c6d1536ea96a68bc13d0d04c12433da5edb
AndysDeepAbstractions <andreas.wienzek@gmail.com>
2017-09-12 09:34:08 +0200

    Merged

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -10,11 +10,12 @@ namespace plt = matplotlibcpp;
 
 using CppAD::AD;
 
+// TODO: Set the timestep length and duration
 // We set the number of timesteps to 25
 // and the timestep evaluation frequency or evaluation
 // period to 0.05.
 size_t N = 25;
-double dt = 0.1;
+double dt = 0.2;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -46,7 +47,7 @@ size_t a_start = delta_start + N - 1;
 
 class FG_eval {
  public:
-  Eigen::VectorXd coeffs;
+  // Fitted polynomial coefficients  Eigen::VectorXd coeffs;
   // Coefficients of the fitted polynomial.
   FG_eval(Eigen::VectorXd coeffs) { this->coeffs = coeffs; }
 
@@ -54,6 +55,10 @@ class FG_eval {
   // `fg` is a vector containing the cost and constraints.
   // `vars` is a vector containing the variable values (state & actuators).
   void operator()(ADvector& fg, const ADvector& vars) {
+    // TODO: implement MPC
+    // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
+    // NOTE: You'll probably go back and forth between this function and
+    // the Solver function below.
     // The cost is stored is the first element of `fg`.
     // Any additions to the cost should be added to `fg[0]`.
     fg[0] = 0;
@@ -152,6 +157,14 @@ vector<double> MPC::Solve(Eigen::VectorXd x0, Eigen::VectorXd coeffs) {
   size_t i;
   typedef CPPAD_TESTVECTOR(double) Dvector;
 
+  // TODO: Set the number of model variables (includes both states and inputs).
+  // For example: If the state is a 4 element vector, the actuators is a 2
+  // element vector and there are 10 timesteps. The number of variables is:
+  //
+  // 4 * 10 + 2 * 9
+  size_t n_vars = 0;
+  // TODO: Set the number of constraints
+  size_t n_constraints = 0;
   double x = x0[0];
   double y = x0[1];
   double psi = x0[2];
@@ -182,7 +195,10 @@ vector<double> MPC::Solve(Eigen::VectorXd x0, Eigen::VectorXd coeffs) {
   // Lower and upper limits for x
   Dvector vars_lowerbound(n_vars);
   Dvector vars_upperbound(n_vars);
+  // TODO: Set lower and upper limits for variables.
 
+  // Lower and upper limits for the constraints
+  // Should be 0 besides initial state.
   // Set all non-actuators upper and lowerlimits
   // to the max negative and positive values.
   for (int i = 0; i < delta_start; i++) {
@@ -231,11 +247,23 @@ vector<double> MPC::Solve(Eigen::VectorXd x0, Eigen::VectorXd coeffs) {
   // Object that computes objective and constraints
   FG_eval fg_eval(coeffs);
 
-  // options
+  //
+  // NOTE: You don't have to worry about these options
+  //
+  // options for IPOPT solver
   std::string options;
+  // Uncomment this if you'd like more print information
   options += "Integer print_level  0\n";
+  // NOTE: Setting sparse to true allows the solver to take advantage
+  // of sparse routines, this makes the computation MUCH FASTER. If you
+  // can uncomment 1 of these and see if it makes a difference or not but
+  // if you uncomment both the computation time should go up in orders of
+  // magnitude.
   options += "Sparse  true        forward\n";
   options += "Sparse  true        reverse\n";
+  // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
+  // Change this as you see fit.
+  options += "Numeric max_cpu_time          0.5\n";
 
   // place to return solution
   CppAD::ipopt::solve_result<Dvector> solution;
@@ -251,6 +279,7 @@ vector<double> MPC::Solve(Eigen::VectorXd x0, Eigen::VectorXd coeffs) {
   bool ok = true;
   ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
 
+  // Cost
   auto cost = solution.obj_value;
   std::cout << "Cost " << cost << std::endl;
   return {solution.x[x_start + 1],   solution.x[y_start + 1],
@@ -259,6 +288,11 @@ vector<double> MPC::Solve(Eigen::VectorXd x0, Eigen::VectorXd coeffs) {
           solution.x[delta_start],   solution.x[a_start]};
 }
 
+  // TODO: Return the first actuator values. The variables can be accessed with
+  // `solution.x[i]`.
+  //
+  // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
+  // creates a 2 element double vector.
 //
 // Helper functions to fit and evaluate polynomials.
 //
