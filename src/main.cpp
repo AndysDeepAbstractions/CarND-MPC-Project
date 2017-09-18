d9ee4718de36b94173b33f0a447b5afcddc2f023
AndysDeepAbstractions <andreas.wienzek@gmail.com>
2017-09-19 01:35:58 +0200

    drives

--- src/main.cpp
+++ src/main.cpp
@@ -118,7 +118,7 @@ int main() {
           dt_0 = std::chrono::high_resolution_clock::now();
           double latency = std::chrono::duration_cast<std::chrono::nanoseconds>(dt_0 - dt_1).count()/10e9;
           //std::cout << "latency : " << latency << "s" << std::endl;
-          latency = max(latency,0.1);
+          latency = 0.5*max(latency,0.1);
           //latency = 0.1;
 
           // try to find inconsistency
